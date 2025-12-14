07ab890c4108fb0ec075c6570c63c361bb92e9a4
oqxo <oqxo.github@gmail.com>
2025-12-14 16:28:02 +0530

    starting go

---
+++ 02.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Hello and welcome to go programming")
+	fmt.Printf("Hello and welcome to %s", "go programming")
+
+	message := "I love go programming"
+	fmt.Println()
+	fmt.Printf("%s\n", message)
+
+}
